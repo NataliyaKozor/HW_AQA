69b979262fbb5140242913d8a7782a745841bf72
NataliyaKozor <107683548+NataliyaKozor@users.noreply.github.com>
2023-01-27 18:58:38 +0300

    Файлы с задниями

---
+++ lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = 'Nataliya'
+print(my_name)
---
+++ lesson_1_task_8.py
@@ -0,0 +1,4 @@
+def figure(num):
+    print(num)
+
+figure(8), figure(8), figure(0), figure(0), figure(5), figure(5), figure(5), figure(3), figure(5), figure(3), figure(5)
