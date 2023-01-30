113c3d3f88bb79eac6fa7ffdccf1638244bcea2b
NataliyaKozor <natalia.kozor@gmail.com>
2023-01-30 22:01:01 +0300

    delete
    ya udalila yo

--- lesson_1_task_1.py
+++
@@ -1,2 +0,0 @@
-my_name = 'Nataliya'
-print(my_name)
--- lesson_1_task_8.py
+++
@@ -1,4 +0,0 @@
-def figure(num):
-    print(num)
-
-figure(8), figure(8), figure(0), figure(0), figure(5), figure(5), figure(5), figure(3), figure(5), figure(3), figure(5)
